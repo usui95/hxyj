8d60944c362c21548660ffd840400fac0ba4d9fd
wjc <wjc@qq.com>
2016-11-12 08:14:39 +0800

    * tijiao

--- resources/views/admin/site/login.blade.php
+++ resources/views/admin/site/login.blade.php
@@ -193,7 +193,10 @@
                         else if (data.msg == "登录成功") {
                             window.location.href = "/admin";
                         }
-                    } 
+                        else{
+                            alert("登录失败");
+                        }
+                    }
                 })
             }
         })
