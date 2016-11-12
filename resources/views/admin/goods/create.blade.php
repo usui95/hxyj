37137eac9848df8ba771541a6303d00af54bd4ee
wjc <wjc@qq.com>
2016-11-12 15:45:43 +0800

    * tijiao

--- resources/views/admin/goods/create.blade.php
+++ resources/views/admin/goods/create.blade.php
@@ -15,31 +15,31 @@
 
         <div class="form-group">
             <label for="name">商品名字:</label>
-            <input type="text" class="form-control" id="name" placeholder=" 请输入商品名字" />
+            <input type="text" class="form-control" id="name" placeholder=" 请输入商品名字"/>
         </div>
         <div class="form-group">
             <label for="nickname">商品别名:</label>
-            <input type="text" class="form-control" id="nickname" placeholder=" 请输入商品别名" />
+            <input type="text" class="form-control" id="nickname" placeholder=" 请输入商品别名"/>
         </div>
         <div class="form-group">
             <label for="description">商品描述:</label>
-           <input type="text" class="form-control" id="description" placeholder=" 请输入商品描述" />
+            <input type="text" class="form-control" id="description" placeholder=" 请输入商品描述"/>
         </div>
         <div class="form-group">
             <label for="price">商品价格:</label>
-            <input type="text" class="form-control" id="price" placeholder=" 请输入商品价格" />
+            <input type="text" class="form-control" id="price" placeholder=" 请输入商品价格"/>
         </div>
         <div class="form-group">
             <label for="category">商品种类:</label>
-            <input type="text" class="form-control" id="category" placeholder=" 请输入商品种类" />
+            <input type="text" class="form-control" id="category" placeholder=" 请输入商品种类"/>
         </div>
         <div class="form-group">
             <label for="score">商品评分:</label>
-            <input type="text" class="form-control" id="score" placeholder=" 请输入商品评分" />
+            <input type="text" class="form-control" id="score" placeholder=" 请输入商品评分"/>
         </div>
         <div class="form-group">
             <label for="comment">商品评价:</label>
-            <input type="text" class="form-control" id="comment" placeholder=" 请输入商品评价" />
+            <input type="text" class="form-control" id="comment" placeholder=" 请输入商品评价"/>
         </div>
 
         <button type="button" id="submit" class="btn btn-default btn-success">提交</button>
@@ -70,9 +70,14 @@
                 data: Json,
                 datatype: 'json',
                 success: function (data) {
-                    console.log(data);
-                    $("#form input").val("");
-//                window.location.reload();
+                    if (data.msg == "该商品已添加，无需重复添加") {
+                        alert(data.msg);
+                    }
+                    else {
+                        alert(data.msg);
+                        $("#form input").val("");
+                        window.location.reload();
+                    }
                 }
             })
         })
--- resources/views/admin/goods/index.blade.php
+++ resources/views/admin/goods/index.blade.php
@@ -64,31 +64,31 @@
             </div>
             <div class="form-group">
                 <label for="name">商品名字:</label>
-                <input type="text" class="form-control" id="name" placeholder=" 请输入商品名字" />
+                <input type="text" class="form-control" id="name" placeholder=" 请输入商品名字"/>
             </div>
             <div class="form-group">
                 <label for="nickname">商品别名:</label>
-                <input type="text" class="form-control" id="nickname" placeholder=" 请输入商品别名" />
+                <input type="text" class="form-control" id="nickname" placeholder=" 请输入商品别名"/>
             </div>
             <div class="form-group">
                 <label for="description">商品描述:</label>
-                <input type="text" class="form-control" id="description" placeholder=" 请输入商品描述" />
+                <input type="text" class="form-control" id="description" placeholder=" 请输入商品描述"/>
             </div>
             <div class="form-group">
                 <label for="price">商品价格:</label>
-                <input type="text" class="form-control" id="price" placeholder=" 请输入商品价格" />
+                <input type="text" class="form-control" id="price" placeholder=" 请输入商品价格"/>
             </div>
             <div class="form-group">
                 <label for="category">商品种类:</label>
-                <input type="text" class="form-control" id="category" placeholder=" 请输入商品种类" />
+                <input type="text" class="form-control" id="category" placeholder=" 请输入商品种类"/>
             </div>
             <div class="form-group">
                 <label for="score">商品评分:</label>
-                <input type="text" class="form-control" id="score" placeholder=" 请输入商品评分" />
+                <input type="text" class="form-control" id="score" placeholder=" 请输入商品评分"/>
             </div>
             <div class="form-group">
                 <label for="comment">商品评价:</label>
-                <input type="text" class="form-control" id="comment" placeholder=" 请输入商品评价" />
+                <input type="text" class="form-control" id="comment" placeholder=" 请输入商品评价"/>
             </div>
             <button type="button" id="button" class="btn btn-default btn-success">确定修改</button>
             &nbsp;&nbsp;
@@ -142,10 +142,9 @@
                 data: Json,
                 datatype: 'json',
                 success: function (data) {
-//                    alert(data.msg);
-                 console.log(data);
+                   alert(data.update);
                     $("#put").hide();
-//                    window.location.reload();
+                    window.location.reload();
                 }
             });
         });
@@ -163,7 +162,7 @@
                     _token: $("input[name='_token']").val()
                 },
                 success: function (data) {
-                    alert(data.msg);
+                    alert(data.del);
                     window.location.reload();
                 }
             });
